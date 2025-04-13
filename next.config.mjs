adf9d3056614e13e73a7f0399f428aed838b8e5c
7medo-77 <ahmedabdelhamid1997@gmail.com>
2025-04-13 19:40:14 +0200

    chore(next): removed .next directory and changed next configuration to allow using image urls from deployed server for use with next Image components

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["nyc3.digitaloceanspaces.com"], // Add your allowed domain here
+    // domains: ["nyc3.digitaloceanspaces.com"], // Add your allowed domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hotelmania.nyc3.cdn.digitaloceanspaces.com",
+        port: "",
+        pathname: "/**",
+        search: "",
+      },
+      {
+        protocol: "https",
+        hostname: "nyc3.digitaloceanspaces.com",
+        port: "",
+        pathname: "/hotelmania/**",
+        search: "",
+      },
+    ],
   },
 };
 
